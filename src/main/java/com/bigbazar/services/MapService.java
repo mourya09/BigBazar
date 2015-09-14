a7167f51a2287cc86e5c440971e35e8356544a1c
PRAVEEN KUMAR <mourya.09@gmail.com>
2015-09-14 13:49:37 +0530

    welcome

--- src/main/java/com/bigbazar/services/MapService.java
+++ src/main/java/com/bigbazar/services/MapService.java
@@ -6,21 +6,11 @@ package com.bigbazar.services;
 import com.vividsolutions.jts.geom.Point;
 
 /**
- * @author NEX6UYU
+ * @author Praveen Kumar
  *
  */
 public interface MapService {
-	/**
-	 * Store customers MUI.
-	 * This method will store the users address. It will store the point which will be the possible destination for 
-	 * users 
-	 * */
-	public Long storeCustomerMUI(Point point);
 	
-	public default Boolean storeBranchLocation(Point point)
-	{
-		return false;
-	}
 	
 
 }
